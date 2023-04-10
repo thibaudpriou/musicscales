18c2aed37e31ecbebe96e8c1eb0d0261e982922a
Thibaud Priou <thibaud.priou@gmail.com>
2023-04-10 15:51:00 +0200

    refactor: del useless GUITAR_PITCH_OFFSET const

--- src/lib/constants/instruments.ts
+++ src/lib/constants/instruments.ts
@@ -1,9 +1,7 @@
 import type { InstrumentInfo } from '$lib/types';
 
-export const GUITAR_PITCH_OFFSET = 4; // lowest pitch is E, which is C + 4 semitones
-
 export const intruments: InstrumentInfo[] = [
-	{ name: 'Bass', value: 'bass', pitchStart: GUITAR_PITCH_OFFSET },
-	{ name: 'Guitar', value: 'guitar', pitchStart: GUITAR_PITCH_OFFSET },
-	{ name: 'Keys', value: 'keyboard' }
+	{ name: 'Bass', value: 'bass', pitchStart: 4 }, // E = C + 4 semitones
+	{ name: 'Guitar', value: 'guitar', pitchStart: 4 }, // E = C + 4 semitones
+	{ name: 'Keys', value: 'keyboard', pitchStart: 0 } // C
 ];
